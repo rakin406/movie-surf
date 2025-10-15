acd1dc55ea4774c2112aa259f95a32cfef4ffb12
Rakin Rahman <rakinhacker@gmail.com>
2025-10-15 23:23:16 +0600

    feat: add trending route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,30 @@
 import { FastifyInstance } from "fastify";
+import { MovieDb } from "moviedb-promise";
 
 async function routes(fastify: FastifyInstance, options) {
+  // Connect to TMDB API
+  let moviedb = new MovieDb("your_api_key");
+
   fastify.get("/", async (request, reply) => {
     return { hello: "world" };
+  });
+
+  // Gets a list of trending movies
+  fastify.get("/trending", async (request, reply) => {
+    let promises = [];
+
+    for (let i = 0; i < 30; ++i) {
+      promises.push(
+        moviedb.trending({
+          media_type: "all",
+          time_window: "week",
+        })
+      );
+    }
+
+    Promise.all(promises).then((values) => {
+      return { movies: values };
+    });
   });
 }
 
