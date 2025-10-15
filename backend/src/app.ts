21bc3aa9c763610cbc2fc549417284f2396a7317
Rakin Rahman <rakinhacker@gmail.com>
2025-10-15 23:45:25 +0600

    feat: enable swagger ui

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,30 @@ const app = Fastify({
 
 await app.register(cors, { origin: "*" });
 
+await app.register(import("@fastify/swagger"));
+
+await app.register(import("@fastify/swagger-ui"), {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
+
 app.register(routes);
 
 export default app;
