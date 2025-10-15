e9d9f6243c28451229b407b5632e318b933b6d0d
Rakin Rahman <rakinhacker@gmail.com>
2025-10-15 14:31:16 +0600

    feat: graceful shutdown

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
+import closeWithGrace from "close-with-grace";
 import app from "./app";
 
 const start = async () => {
+  // Delay is the number of milliseconds for the graceful close to finish
+  // TODO: Use a constant delay set by environment variable.
+  closeWithGrace({ delay: 500 }, async ({ err }) => {
+    if (err != null) {
+      app.log.error(err);
+    }
+
+    await app.close();
+  });
+
+  await app.ready();
+
   try {
     await app.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
