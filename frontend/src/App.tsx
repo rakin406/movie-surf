0b1726bade2d307ce8fd997810f9b4b5b045aeed
Rakin Rahman <rakinhacker@gmail.com>
2025-10-16 22:53:16 +0600

    feat: center container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
-import Container from "@mui/material/Container";
+import { CircularProgress, Container } from "@mui/material";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState();
 
   useEffect(() => {
     fetch("/api/v1")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message))
+      .then((data) => {
+        setMessage(data.message);
+        setLoading(false);
+      })
       .catch(console.error);
   }, []);
 
-  return <Container maxWidth="sm">{message}</Container>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "98vh",
+      }}
+    >
+      {loading ? <CircularProgress size="3rem" /> : message}
+    </Container>
+  );
 }
 
 export default App;
