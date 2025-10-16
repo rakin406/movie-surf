db4afbec2b929565bed495d6f0e7e002c16a1dd7
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 01:17:26 +0600

    feat: use Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,10 @@
-import { useState, useEffect } from "react";
-import { CircularProgress, Container } from "@mui/material";
+import { Container } from "@mui/material";
+import Home from "./components/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState();
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/v1/trending")
-      .then((res) => res.json())
-      .then((data) => {
-        setMessage(data.message);
-        setLoading(false);
-      })
-      .catch(console.error);
-  }, []);
-
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "98vh",
-      }}
-    >
-      {loading ? <CircularProgress size="3rem" /> : message}
+    <Container maxWidth="sm">
+      <Home />
     </Container>
   );
 }
