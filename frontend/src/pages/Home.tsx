632c687f26610c7c70406d6e789c126f096e344d
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 20:15:13 +0600

    refactor: put loading logic in separate component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,26 +1,10 @@
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
-
 import ShowMovies from "../components/ShowMovies";
+import Loading from "../components/Loading";
 import { useFetch } from "../hooks/useFetch";
 
 function Home() {
   const trending = useFetch("http://localhost:3000/api/v1/trending");
-
-  return trending ? (
-    <ShowMovies movies={trending} />
-  ) : (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "85vh",
-      }}
-    >
-      <CircularProgress size="3rem" />
-    </Box>
-  );
+  return trending ? <ShowMovies movies={trending} /> : <Loading />;
 }
 
 export default Home;
