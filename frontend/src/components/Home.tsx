792512bbf4a08fcf2033b785006d7b82be26d5b2
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 12:50:30 +0600

    feat: display trending movies

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,37 @@
-import { CircularProgress, Box } from "@mui/material";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 import { useFetch } from "../hooks/useFetch";
 
 function Home() {
   const trending = useFetch("http://localhost:3000/api/v1/trending");
 
   return trending ? (
-    trending
+    // TODO: Avoid using "any" for type.
+    <ImageList sx={{ width: 500, height: 450 }}>
+      {trending["movies"].map((movie: any) => (
+        <ImageListItem key={movie.poster}>
+          <img
+            srcSet={`${movie.poster}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${movie.poster}?w=248&fit=crop&auto=format`}
+            alt={movie.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            sx={{
+              background:
+                "linear-gradient(to top, rgba(0,0,0,0.7) 0%, " +
+                "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+              textAlign: "center",
+            }}
+            title={movie.title}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
   ) : (
     <Box
       sx={{
