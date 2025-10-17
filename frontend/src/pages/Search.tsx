500d197fcdbec3df239a0341df5aa6ae2fbd314e
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 23:31:09 +0600

    fix: put query as dependency

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,11 +1,18 @@
+import { useSearchParams } from "react-router-dom";
+
 import ShowMovies from "../components/ShowMovies";
 import Loading from "../components/Loading";
 import { useFetch } from "../hooks/useFetch";
 
-function Search({ title }: { title: string }) {
+function Search() {
+  const [searchParams] = useSearchParams();
+  // TODO: Handle no query.
+  const query = searchParams.get("q") as string;
   const movies = useFetch(
-    `http://localhost:3000/api/v1/search?q=${encodeURIComponent(title)}`
+    `http://localhost:3000/api/v1/search?q=${encodeURIComponent(query)}`,
+    query
   );
+
   return movies ? <ShowMovies movies={movies} /> : <Loading />;
 }
 
