143baff0f48a7da9c9c16e470fae96c17f34b96b
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 23:30:13 +0600

    refactor: put NavigationBar outside of routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import Container from "@mui/material/Container";
 
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <Container maxWidth="xl">
+      <NavigationBar />
       <Routes>
-        <Route path="/" element={<NavigationBar />}>
-          <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+
+        {/* <Route path="about" element={<About />} />
           <Route path="dashboard" element={<Dashboard />} /> */}
 
-          {/* Using path="*"" means "match anything", so this route
+        {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </Container>
   );
