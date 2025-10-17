4625450a5da4088e47912eb65dd61c4d2d90710f
Rakin Rahman <rakinhacker@gmail.com>
2025-10-18 00:00:49 +0600

    refactor: dont fetch if query is empty

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,4 +1,5 @@
 import { useSearchParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
 import ShowMovies from "../components/ShowMovies";
 import Loading from "../components/Loading";
@@ -7,11 +8,28 @@ import { useFetch } from "../hooks/useFetch";
 function Search() {
   const [searchParams] = useSearchParams();
   // TODO: Handle no query.
-  const query = searchParams.get("q") as string;
+  const query = searchParams.get("q") ?? "";
   const movies = useFetch(
-    `http://localhost:3000/api/v1/search?q=${encodeURIComponent(query)}`,
+    query
+      ? `http://localhost:3000/api/v1/search?q=${encodeURIComponent(query)}`
+      : "",
     query
   );
+
+  if (!query) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "85vh",
+        }}
+      >
+        Invalid Search
+      </Box>
+    );
+  }
 
   return movies ? <ShowMovies movies={movies} /> : <Loading />;
 }
