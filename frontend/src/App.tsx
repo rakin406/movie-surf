cec43021b846c10b2b1daf581d8baa8c6ef28586
Rakin Rahman <rakinhacker@gmail.com>
2025-10-17 17:37:11 +0600

    feat: use router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import { Container } from "@mui/material";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Container maxWidth="sm">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Container>
   );
 }
